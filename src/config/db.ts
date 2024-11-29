62796d91ca22a0f287f3f3892675870cbc76fa80
Shubham Verma <shubhamverma6351@gmail.com>
2024-11-29 23:09:14 +0530

    chore: update dependencies for MongoDB and Mongoose, and refactor server initialization to include database connection

---
+++ src/config/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "config";
+import logger from "./logger";
+
+const connectDB = async () => {
+    try {
+        const mongoURI: string = config.get("database.url");
+        await mongoose.connect(mongoURI);
+        logger.info("Connected to MongoDB");
+    } catch (error) {
+        if (error instanceof Error) {
+            logger.error("MongoDB connection error: " + error.message);
+            process.exit(1);
+        }
+    }
+};
+
+export default connectDB;
