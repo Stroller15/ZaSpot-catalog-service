95f3152e8e371e63431395af4229e63c9a44c9a8
Shubham Verma <shubhamverma6351@gmail.com>
2024-11-30 17:27:59 +0530

    feat: add Category model with price configuration and attributes schema for MongoDB

---
+++ src/modules/category/category.model.ts
@@ -0,0 +1,116 @@
+import mongoose from "mongoose";
+
+export interface PriceConfiguration {
+    [key: string]: {
+        priceType: "base" | "additional";
+        availableOptions: string[];
+    };
+}
+
+export interface Attribute {
+    name: string;
+    widgetType: "switch" | "radio";
+    defaultValue: string;
+    availableOptions: string[];
+}
+
+export interface Category {
+    name: string;
+    priceConfiguration: PriceConfiguration;
+    attributes: Attribute[];
+}
+
+const PriceConfigurationSchema = new mongoose.Schema<PriceConfiguration>({
+    priceType: {
+        type: String,
+        enum: ["base", "additional"],
+        required: true,
+    },
+    availableOptions: {
+        type: [String],
+        required: true,
+    },
+});
+
+const attributesSchema = new mongoose.Schema<Attribute>({
+    name: {
+        type: String,
+        required: true,
+    },
+    widgetType: {
+        type: String,
+        enum: ["switch", "radio"],
+        required: true,
+    },
+    defaultValue: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true,
+    },
+    availableOptions: {
+        type: [String],
+        required: true,
+    },
+});
+
+const categorySchema = new mongoose.Schema<Category>({
+    name: {
+        type: String,
+        required: true,
+    },
+    priceConfiguration: {
+        type: Map,
+        of: PriceConfigurationSchema,
+        required: true,
+    },
+    attributes: {
+        type: [attributesSchema],
+        required: true,
+    },
+});
+
+export default mongoose.model("Category", categorySchema);
+
+
+// This is for imagination how model will look like
+// {
+//     "name": "Pizza",
+//     "priceConfiguration": {
+//         "Size": {
+//             "priceType": "base",
+//             "availableOptions": [
+//                 "small",
+//                 "medium",
+//                 "large"
+//             ]
+//         },
+//         "crust": {
+//             "priceType": "additional",
+//             "avaibleOptions": [
+//                 "Thin",
+//                 "Thick"
+//             ]
+//         }
+
+//     },
+//     "attributes": [
+//         {
+//             "name": "isHit",
+//             "widgetType": "switch",
+//             "defaultValue": "No",
+//             "availableOptions": [
+//                 "yes",
+//                 "no"
+//             ]
+//         },
+//         {
+//             "name": "spiciness",
+//             "widgetType": "radio",
+//             "defaultValue": "medium",
+//             "avaibleOptions": [
+//                 "less",
+//                 "medium",
+//                 "hot"
+//             ]
+//         }
+//     ]
+// }
